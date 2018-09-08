ee3b9d5f75d3fbc2a4d703e7ff1d21102afb35b1
naj21 <38796649+naj21@users.noreply.github.com>
2018-09-08 15:51:23 +0100

    Update UserController.js

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -43,61 +43,6 @@ module.exports = {
 				return res.send(err);
 			}
 			if(createUser){
-				var twilio = require('twilio');
-				var accountSid = 'ACf31ec4d84cb67a260277c41dd5c4be7c';
-				var authToken = 'e392928a870e85bc94825d546f867be3'
-				var client = new twilio(accountSid, authToken);
-
-				client.messages.create({
-					body: `Hello, ${details.firstName}, from NAJ`,
-					to: '+2349081459499',
-					from: '+15867880990'
-				})
-				.then(message=> console.log('Twilio - ', message.sid));
-
-
-				const AWS = require('aws-sdk');
-
-				AWS.config.update({
-					acesssKeyId: 'AKIAIP6DUUXB7SUIRZVQ',
-					secreteKeyId: '9/NyYd1G5gKRBxcGCG+qs0RPlR7lkDsHhP+hb0CX',
-					region: 'us-east-1'
-				});
-
-				const ses = new AWS.SES();
-				const params = {
-					Destination: {
-						ToAddresses: ['jesax013@gmail.com']
-					},
-					ConfigurationSetName: 'Feedback',
-					Message: {
-						Body: {
-							Html: {
-								Charset: 'UTF-8',
-								Data: '<html><body><h1>Hello Jessica</h1></body></html>'
-							},
-							Text: {
-								Charset: 'UTF-8',
-								Data: 'Hello Jessica Sample description'
-							}
-						},
-						Subject: {
-							Charset: 'UTF-8',
-							Data: 'Order Confirmation'
-						}
-					},
-					Source: 'jesax013@gmail.com'
-				}
-
-				const sendEmail = ses.sendEmail(params).promise();
-
-				sendEmail
-					.then(data => {
-						console.log('Email submitted to SES', data);
-					})
-					.catch(error => {
-						console.log(error)
-					});	
 
 				return res.send({
 					message:'Created Successfully', type:'success'
